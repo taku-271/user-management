6188bf3e756564aa56cf3201caed021a4f49af2d
taku-271 <ta.yamashita.314@gmail.com>
2024-03-18 03:16:37 +0900

    fix: gitignore

---
+++ packages/backend/src/graphql/src/resolvers/Query/user.ts
@@ -0,0 +1,27 @@
+import type { User } from "@prisma/client";
+import type { QueryResolvers } from "./../../../types.generated";
+export const user: NonNullable<QueryResolvers["user"]> = async (
+  _parent,
+  _arg,
+  _ctx
+) => {
+  const user = await _ctx.prisma.user.findUnique({
+    where: { id: _arg.id },
+  });
+
+  if (!user) {
+    throw new Error("User not found");
+  }
+
+  return convertUser(user);
+};
+
+const convertUser = (user: User) => ({
+  id: user.id,
+  name: user.name,
+  email: user.email,
+  password: user.password,
+  isAdmin: user.is_admin,
+  createdAt: user.created_at,
+  updatedAt: user.updated_at,
+});
